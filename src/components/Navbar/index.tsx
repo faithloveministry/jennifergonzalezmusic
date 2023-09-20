2ab3c1ec12b3dad0fd6ae6fb30cf017beb740b84
Caleb Hoff <crunchycat1@gmail.com>
2023-09-20 02:02:31 -0500

    height fix

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import styles from '@/components/Navbar/Navbar.module.scss';
 
 export const Navbar = (props: ParentProps) => (
   <>
-    <style>{`.active{color:#000}.inactive{color:#333;text-decoration:none}`}</style>
+    <style>{`.active{color:#2d0c28}.inactive{color:#42213d;text-decoration:none}`}</style>
     <div class={styles.Navbar}>{props.children}</div>
   </>
 );
