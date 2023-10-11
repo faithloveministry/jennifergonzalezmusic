7b68c2b8d5daba000c228d444205cc2b8f9a1052
Caleb Hoff <crunchycat1@gmail.com>
2023-10-11 02:00:28 -0500

    dont mangle event names

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default ({ mode }: { mode: 'production' | 'development' | 'test' }) => {
           ? {
               compress: { arguments: true, ecma: 2020, hoist_funs: true, passes: 3, pure_getters: true, unsafe: true, unsafe_arrows: true, unsafe_comps: true, unsafe_symbols:true }, //prettier-ignore
               format: { comments: false, ecma: 2020, wrap_func_args: false },
-              mangle: { properties: { regex: /^(?:owned|cleanups|owner|observerSlots|comparator|sourceSlots|running|disposed|tState|updatedAt|pure|suspense|inFallback|effects|_\$host|\$\$click|numPages|\$\$touchend|_v\$|_v\$2|_v\$3|_v\$4|_v\$5|_v\$6|_v\$7|_v\$8|_v\$9|_v\$10)$/ } }, //prettier-ignore
+              mangle: { properties: { regex: /^(?:owned|cleanups|owner|observerSlots|comparator|sourceSlots|running|disposed|tState|updatedAt|pure|suspense|inFallback|effects|_\$host|numPages|_v\$|_v\$2|_v\$3|_v\$4|_v\$5|_v\$6|_v\$7|_v\$8|_v\$9|_v\$10)$/ } }, //prettier-ignore
             }
           : undefined,
       modulePreload: { polyfill: false }, // Delete this line if outputting more than 1 chunk
